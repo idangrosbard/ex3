9a1dec59cd3633320806905bf5bf36a74a282f47
idg7 <idg7722@gmail.com>
2022-12-15 18:43:30 +0200

    Fixed error messages

--- message_reader.c
+++ message_reader.c
@@ -15,47 +15,56 @@ int main(int argc, char ** argv) {
     int ret_val;
     char * message_slot_file_path, * message;
 
+    // Check arguments
     if (argc != 3) {
-        perror(strerror(EINVAL));
+    	errno = EINVAL;
+        perror("");
         exit(1);
     }
     message_slot_file_path = argv[1];
     channel = strtoul(argv[2], NULL, 10);
     
+    // Check channel
     if (channel == 0) {
-        perror(strerror(EINVAL));
+    	errno = EINVAL;
+        perror("");
         exit(1);
     }
 	
+    // Open message slot
     message_slot = open( message_slot_file_path, O_RDONLY );
     if( message_slot < 0 ) {
-        perror(strerror(errno));
+        perror("");
         exit(1);
     }
     
+    // Set channel
     ret_val = ioctl( message_slot, MSG_SLOT_CHANNEL, channel);
     if (ret_val < 0) {
-        perror(strerror(errno));
+        perror("");
         exit(1);
     }
     
+    // Read message
     message = malloc(129 * sizeof(char));
     message[128] = '\0';
     ret_val = read( message_slot, message, 128);
     if (ret_val < 0) {
-        perror(strerror(errno));
+        perror("");
         exit(1);
     }
 	
+    // Close message slot
     ret_val = close(message_slot);
     if (ret_val < 0) {
-        perror(strerror(errno));
+        perror("");
         exit(1);
     }
 	
+    // Print message
     ret_val = write(1, message, 128);
     if (ret_val < 0) {
-    	perror(strerror(errno));
+    	perror("");
     }
     free(message);
 	
--- message_sender.c
+++ message_sender.c
@@ -14,41 +14,49 @@ int main(int argc, char ** argv) {
     int ret_val;
     char * message_slot_file_path, * message;
 
+    // Check arguments
     if (argc != 4) {
-    	perror(strerror(EINVAL));
+    	errno = EINVAL;
+    	perror("");
         exit(1);
     }
     message_slot_file_path = argv[1];
     channel = strtoul(argv[2], NULL, 10);
     
+    // Check channel
     if (channel == 0) {
-       	perror(strerror(EINVAL));
+    	errno = EINVAL;
+       	perror("");
         exit(1);
     }
 
     message = argv[3];
 
+    // Open message slot
     message_slot = open( message_slot_file_path, O_RDWR );
     if( message_slot < 0 ) {
-       	perror(strerror(errno));
+       	perror("");
         exit(1);
     }
 
+    // Set channel
     ret_val = ioctl( message_slot, MSG_SLOT_CHANNEL, channel);
     if (ret_val < 0) {
-      	perror(strerror(errno));
+      	perror("");
         exit(1);
     }
 
+    // Write message
     ret_val = write( message_slot, message, strlen(message));
     if (ret_val < 0) {
-       	perror(strerror(errno));
+       	perror("");
         exit(1);
     }
 
+    // Close message slot
     ret_val = close( message_slot );
     if (ret_val < 0) {
-    	perror(strerror(errno));
+    	perror("");
         exit(1);
     }
 
